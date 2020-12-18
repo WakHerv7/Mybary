7bc9fdd01528fe5a57adab402581d33a29e67fa2
WakHerv7 <wakam.hermann@gmail.com>
2020-12-19 00:55:37 +0100

    Finish Project

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,14 +1,32 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-);
+// Since we are using @import inside our css 
+// That imported css get loaded after main.css get loaded and after the document get loaded
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 150 ,
-    imageResizeTargetWidth: 150,
-    imageResizeTargetHeight: 150,
-});
+const rootStyles = window.getComputedStyle(document.documentElement); // To check when css files get loaded
 
-FilePond.parse(document.body);
-console.log('file Upload is working !');
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null) {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready) ;
+}
+
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'));     // parseFloat: To convert the output from String to Float
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'));
+    const coverHeight = coverWidth / coverAspectRatio ;
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    );
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio ,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight,
+    });
+    
+    FilePond.parse(document.body);
+    console.log('file Upload is working !');
+}
+
